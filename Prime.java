b28d4ab25c6a40f047ea66b688583c81b499c7f3
sonnguyenyhct <31002371+sonnguyenyhct@users.noreply.github.com>
2020-09-10 10:34:00 +0700

    add

---
+++ Prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        int prime;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao 1 so nguyen duong");
+        prime = scanner.nextInt();
+        if (checkPrime(prime)){
+            System.out.println(prime + "la so nguyen to");
+        }else {
+            System.out.println(prime + "khong la so nguyen to");
+        }
+
+    }
+    public static boolean checkPrime(int prime){
+        for (int count = 2; count < Math.sqrt(prime); count++){
+            if (prime%count == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
